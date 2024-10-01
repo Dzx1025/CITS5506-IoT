774a29ee6adc0b2ef001dd1ac852cf7d9f288e7c
Dzx <zexuding1025@gmail.com>
2024-10-01 16:34:15 +0800

    Refactor code to improve MQTT and InfluxDB integration

--- smart_iv_bag/smart_iv_bag.ino
+++ smart_iv_bag/smart_iv_bag.ino
@@ -1,20 +1,31 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
+#include <ArduinoJson.h>
 #include <HX711.h>
+#include <InfluxDbClient.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
 
 // Wi-Fi settings
-const char* ssid = "*******";
-const char* password = "********";
+const char *ssid = "*******";
+const char *password = "********";
+
+WiFiClient espClient;
 
 // MQTT broker details
-const char* mqtt_server = "test.mosquitto.org";
+PubSubClient mqtt_client(espClient);
+
+const char *mqtt_server = "test.mosquitto.org";
 const int mqtt_port = 1883;
-const char* mqtt_topic = "ivbag/40/testnum";
+const char *mqtt_topic = "ivbag/40/testnum";
 
-// MQTT client and WiFi client instances
-WiFiClient espClient;
-PubSubClient client(espClient);
+// InfluxDB details
+const char *influxdb_url = "http://localhost:8086";
+const char *influxdb_db_name = "your_database";
+const char *influxdb_user = "your_username";
+const char *influxdb_password = "your_password";
 
+InfluxDBClient db_client(influxdb_url, influxdb_db_name);
+
+// HX711 settings
 HX711 scale;
 const int DOUT = 13;
 const int HX711_SCK = 14;
@@ -22,14 +33,19 @@ const int HX711_SCK = 14;
 float calibration_factor = -7050;
 float full_bottle_weight = 500; // Default value, can be changed via button
 bool isFullBottleSet = false;
-int level = 100;
+float lastValidWeight = full_bottle_weight;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 500; // 500 milliseconds debounce delay
 
 // Pin for the button to set full bottle weight
-const int buttonPin = 2;  
-int buttonState = 0;       
+const int buttonPin = 2;
+int buttonState = 0;
 
 // Pin for the LED
-const int ledPin = 25;  
+const int ledPin = 25;
+
+// Level of the IV bag
+int level = 100;
 
 void setup() {
   Serial.begin(115200);
@@ -37,62 +53,40 @@ void setup() {
   // Initialize HX711
   scale.begin(DOUT, HX711_SCK);
   scale.set_scale(calibration_factor);
-  scale.tare(); 
+  scale.tare();
 
   // Set up the button
   pinMode(buttonPin, INPUT_PULLUP);
-
   // Set up the LED
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  
+
   setup_wifi();
-  client.setServer(mqtt_server, mqtt_port);
+  mqtt_client.setServer(mqtt_server, mqtt_port);
+
+  setupInfluxDB();
 }
 
 void loop() {
-  if (!client.connected()) {
+  if (!mqtt_client.connected()) {
     reconnect();
   }
-  client.loop();
+  mqtt_client.loop();
 
   buttonState = digitalRead(buttonPin);
 
   // Check if button is pressed to set full bottle weight
-  if (buttonState == LOW) {
-    full_bottle_weight = -scale.get_units();
-    isFullBottleSet = true;
-    Serial.println("Full bottle weight set to: " + String(full_bottle_weight));
-
-    // Turn on LED for 1 second after full bottle weight is set
-    digitalWrite(ledPin, HIGH); 
-    delay(1000);             
-    digitalWrite(ledPin, LOW);
-
-    delay(500); // Debounce
+  if (isButtonPressed()) {
+    setFullBottleWeight();
   }
 
   // Read weight from sensor
-  float current_weight = -scale.get_units();
-  Serial.print("currentweight: ");
-  Serial.println(current_weight);
+  float current_weight = readWeightFromSensor();
 
-  if (!isFullBottleSet) {
-    level = 100;
-    Serial.println("Please set the full bottle weight by pressing the button.");
-  } else if (full_bottle_weight != 0) {
-    // Calculate level as a percentage
-    level = (current_weight / full_bottle_weight) * 100;
-    level = constrain(level, 0, 100); // Ensure level is between 0 and 100
-  } else {
-    level = 0;
+  calculateLevel(current_weight);
+  if (isFullBottleSet) {
+    publishLevelData();
   }
-
-  // Publish level data to the topic
-  String payload = "{\"level\": " + String(level) + "}";
-  Serial.print("Publishing message: ");
-  Serial.println(payload);
-  client.publish(mqtt_topic, payload.c_str());
 
   delay(1000);
 }
@@ -119,17 +113,97 @@ void setup_wifi() {
 
 // Function to reconnect to MQTT broker if the connection is lost
 void reconnect() {
-  while (!client.connected()) {
+  while (!mqtt_client.connected()) {
     Serial.print("Attempting MQTT connection...");
     String clientId = "TTGO-TBeam-";
     clientId += String(random(0xffff), HEX);
-    if (client.connect(clientId.c_str())) {
+    if (mqtt_client.connect(clientId.c_str())) {
       Serial.println("connected");
     } else {
       Serial.print("failed, rc=");
-      Serial.print(client.state());
+      Serial.print(mqtt_client.state());
       Serial.println(" try again in 2 seconds");
       delay(2000);
     }
+  }
+}
+
+void setupInfluxDB() {
+  db_client.setConnectionParamsV1(influxdb_url, influxdb_db_name, influxdb_user, influxdb_password);
+
+  if (db_client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(db_client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(db_client.getLastErrorMessage());
+  }
+}
+
+bool isButtonPressed() { 
+  return digitalRead(buttonPin) == LOW; 
+}
+
+float readWeightFromSensor() {
+  unsigned long currentTime = millis();
+  if (currentTime - lastDebounceTime > debounceDelay) {
+    lastDebounceTime = currentTime;
+    float weight = -scale.get_units();
+    Serial.print("current weight: ");
+    Serial.println(weight);
+    lastValidWeight = weight; // Update last valid weight value
+    return weight;
+  }
+  return lastValidWeight;
+}
+
+void setFullBottleWeight() {
+  full_bottle_weight = readWeightFromSensor();
+  isFullBottleSet = true;
+  Serial.println("Full bottle weight set to: " + String(full_bottle_weight));
+
+  // Turn on LED for 1 second after full bottle weight is set
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+}
+
+void calculateLevel(float current_weight) {
+  if (!isFullBottleSet) {
+    level = 100;
+    Serial.println("Please set the full bottle weight by pressing the button.");
+  } else if (full_bottle_weight != 0) {
+    level = (current_weight / full_bottle_weight) * 100;
+    level = constrain(level, 0, 100); // Ensure level is between 0 and 100
+  } else {
+    level = 0;
+  }
+}
+
+void publishLevelData() {
+  // Create a JSON document
+  StaticJsonDocument<200> data;
+  data["level"] = level;
+  // Serialize JSON document to string
+  String payload;
+  serializeJson(data, payload);
+
+  Serial.print("Publishing message: ");
+  Serial.println(payload);
+  mqtt_client.publish(mqtt_topic, payload.c_str());
+
+  // Write data to InfluxDB
+  writeDataToInfluxDB(level);
+}
+
+void writeDataToInfluxDB(int level) {
+  Point sensorData("monitor variables");
+  sensorData.addField("weight", level);
+
+  if (db_client.writePoint(sensorData)) {
+    Serial.println("Data written to InfluxDB");
+  } else {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(mqtt_client.getLastErrorMessage());
   }
 }
